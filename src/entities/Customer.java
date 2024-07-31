838de5cb6dea6c0940855265692db562855dd9bf
MelRecalde <melquisedicrecalde@gmail.com>
2024-07-31 16:02:03 +0200

    classes Product/Order/Customer

--- src/entities/Customer.java
+++ src/entities/Customer.java
@@ -1,5 +1,25 @@
 package entities;
 
 public class Customer {
+    private String id;
+    private String name;
+    private Integer tier;
 
+    public Customer(String id, String name, Integer tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
 }
